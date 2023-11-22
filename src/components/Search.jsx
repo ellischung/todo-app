39c4f784fdf9b991a4e742e6b0d403036065be3a
Ellis <ellischung98@gmail.com>
2023-11-22 02:13:19 -0500

    sizing of top inputs needs work

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -12,7 +12,7 @@ function Search() {
     <div className="pt-6 pb-3">
       <input
         type="text"
-        className="bg-secondary font-bold placeholder-tag rounded-full border p-2 w-[80%] sm:w-[24%]"
+        className="bg-secondary font-bold placeholder-tag rounded-full border p-2 w-[80%] sm:w-[40%]"
         placeholder="Search"
         onChange={handleChange}
       />
